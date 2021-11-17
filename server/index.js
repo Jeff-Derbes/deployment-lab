cead7326356a9b18d089cafa7e26281132114cef
Jeff-Derbes <derbes.jeff@gmail.com>
2021-11-17 14:10:40 -0600

    fix

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html '))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 
