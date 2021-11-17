e4af7a701c54cd35f20d383df370d1dca636641b
Jeff-Derbes <derbes.jeff@gmail.com>
2021-11-17 15:50:15 -0600

    added styles

--- server/index.js
+++ server/index.js
@@ -4,10 +4,7 @@ const app = express();
 
 const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
-})
-
+app.use(express.static(path.join(__dirname, '../public')))
 
 
 
